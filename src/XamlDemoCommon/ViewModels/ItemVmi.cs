0adafca2075444147be5efdda69dca1883689c1e
Andreas Hammar <andreas.hammar@jayway.com>
2014-11-13 16:59:39 +0100

    added proj with viewmodels

---
+++ src/XamlDemoCommon/ViewModels/ItemVmi.cs
@@ -0,0 +1,9 @@
+﻿namespace XamlDemoCommon.ViewModels
+{
+    public class ItemVmi
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUri { get; set; }
+    }
+}
---
+++ src/XamlDemoCommon/ViewModels/MainPageVm.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Web.Syndication;
+using XamlDemoCommon.Services;
+
+namespace XamlDemoCommon.ViewModels
+{
+    public class MainPageVm : VmBase
+    {
+        public string Title { get; set; }
+        public IList<ItemVmi> Items { get; set; }
+
+        public async Task InitAsync()
+        {
+            Title = "main page title";
+            var feedItems = await new DataService().GetAsync();
+            Items = feedItems.Select(CreateItemVmi).ToList();
+        }
+
+        private ItemVmi CreateItemVmi(SyndicationItem item)
+        {
+            return new ItemVmi
+            {
+                Title = item.Title.Text,
+                Description = item.Content.Text,
+                ImageUri = item.ItemUri.AbsoluteUri
+            };
+        }
+    }
+}
