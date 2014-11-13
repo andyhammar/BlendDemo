c1b36165a4da146228389aee5f68d1ad6c5a9aaa
Andreas Hammar <andreas.hammar@jayway.com>
2014-11-13 17:51:15 +0100

    common lib in place

---
+++ src/BlendDemoPrep/BlendDemoCommon/ViewModels/ItemVmi.cs
@@ -0,0 +1,10 @@
+﻿namespace XamlDemoCommon.ViewModels
+{
+    public class ItemVmi
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUri { get; set; }
+        public string Date { get; set; }
+    }
+}
