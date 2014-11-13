5651b26131bf21d8f769636f4a8a5c2f43e990eb
Andreas Hammar <andreas.hammar@jayway.com>
2014-11-13 21:47:52 +0100

    gridview in place

--- src/XamlDemoCommon/ViewModels/ItemVmi.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace XamlDemoCommon.ViewModels
-{
-    public class ItemVmi
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string ImageUri { get; set; }
-    }
-}
