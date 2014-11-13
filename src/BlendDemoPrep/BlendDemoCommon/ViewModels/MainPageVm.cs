c03eb7ef9b233f2147b744c11afa0635c622b35d
Andreas Hammar <andreas.hammar@jayway.com>
2014-11-13 21:56:10 +0100

    jayway image

--- src/BlendDemoPrep/BlendDemoCommon/ViewModels/MainPageVm.cs
+++ src/BlendDemoPrep/BlendDemoCommon/ViewModels/MainPageVm.cs
@@ -28,7 +28,8 @@ namespace XamlDemoCommon.ViewModels
             {
                 Title = item.Title.Text,
                 Description = item.Summary.Text,
-                Date = item.PublishedDate.ToString("HH:mm")
+                Date = item.PublishedDate.ToString("HH:mm"),
+                ImageUri = "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xap1/v/t1.0-1/p160x160/1380687_522238147867581_92872882_n.png?oh=604b2b67f73f586ba2a889c45a997380&oe=55173A47&__gda__=1423782905_bf9f5160c989cac21e9d699b24d2810d"
             };
         }
     }
