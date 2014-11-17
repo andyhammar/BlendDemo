d6962452e0b0b1c96f5387cd735135145ffacb11
Andreas Hammar <andreas.hammar@jayway.com>
2014-11-17 15:30:23 +0100

    sampledata implemented

--- src/BlendDemoPrep/BlendDemoCommon/SampleData/MainPageVmSampleData.cs
+++ src/BlendDemoPrep/BlendDemoCommon/SampleData/MainPageVmSampleData.cs
@@ -7,30 +7,35 @@ using BlendDemoCommon.ViewModels;
 
 namespace BlendDemoCommon.SampleData
 {
-    public class MainPageVm : VmBase
+    public class MainPageVmSampleData : MainPageVm
     {
-        public string Title { get; set; }
-        public IList<ItemVmi> Items { get; set; }
+        private static int _itemCount;
 
-        public MainPageVm()
+        public MainPageVmSampleData()
         {
-            Title = "main page title";
+            Title = "sampledata main page title";
+            Items = new ItemVmi[]
+            {
+                CreateItemVmi("news item 1", "short text"),
+                CreateItemVmi("news item 2", "Bacon ipsum dolor amet salami sirloin picanha strip steak shank turkey. Meatloaf ground round bacon, pancetta sausage prosciutto biltong pig ham hock leberkas turkey. Beef ribs chicken short ribs jowl leberkas sirloin frankfurter salami venison hamburger jerky. Swine bacon ribeye jerky, strip steak salami turkey ham hock sirloin. Chuck pig strip steak, pork loin cow ham hock bacon meatball frankfurter ground round. Spare ribs pork loin beef beef ribs venison. Bresaola landjaeger hamburger rump alcatra pork chop pork loin pig tri-tip drumstick brisket pastrami venison."),
+                CreateItemVmi("news item with very veeeeeeeeeeery long title ", "short text"),
+                CreateItemVmi("news item 4", "short text"),
+                CreateItemVmi("news item 5", "short text"),
+            };
         }
 
-        public async Task InitAsync()
+        private ItemVmi CreateItemVmi(string title, string description)
         {
-            var feedItems = await new DataService().GetAsync();
-            Items = feedItems.Select(CreateItemVmi).ToList();
-        }
+            _itemCount++;
+            var imageUri = _itemCount % 2 == 0 ?
+                "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xap1/v/t1.0-1/p160x160/1380687_522238147867581_92872882_n.png?oh=604b2b67f73f586ba2a889c45a997380&oe=55173A47&__gda__=1423782905_bf9f5160c989cac21e9d699b24d2810d" :
+                "http://filmfork-cdn.s3.amazonaws.com/content/TopGun1.jpg";
 
-        private ItemVmi CreateItemVmi(SyndicationItem item)
-        {
             return new ItemVmi
             {
-                Title = item.Title.Text,
-                Description = item.Summary.Text,
-                Date = item.PublishedDate.ToString("HH:mm"),
-                ImageUri = "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xap1/v/t1.0-1/p160x160/1380687_522238147867581_92872882_n.png?oh=604b2b67f73f586ba2a889c45a997380&oe=55173A47&__gda__=1423782905_bf9f5160c989cac21e9d699b24d2810d"
+                Title = title,
+                Description = description,
+                ImageUri = imageUri
             };
         }
     }
